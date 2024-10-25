2037c28aba346996828fdf224ec5c68581af3ad2
Vitor400 <kawanvitor007@gmail.com>
2024-10-25 20:10:53 -0300

    Criando modelo para evento

---
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -0,0 +1,42 @@
+package ifrn.pi.eventos.models;
+
+public class Evento {
+
+	private String nome;
+	private String local;
+	private String data;
+	private String hoario;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHoario() {
+		return hoario;
+	}
+
+	public void setHoario(String hoario) {
+		this.hoario = hoario;
+	}
+
+}
