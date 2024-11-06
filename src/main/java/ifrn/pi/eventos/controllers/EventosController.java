bf33aba2aa26e7bb3054ebd2bcf1d097a9873a08
Kawan Vitor De França Dos Santos <k.vitor@academico.ifrn.edu.br>
2024-11-06 07:26:48 -0300

    atualização

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -6,11 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.pi.eventos.models.Evento;
@@ -23,7 +21,7 @@ public class EventosController {
 	@Autowired
 	private EventoRepository er;
 
-	@GetMapping("eventos/form")
+	@GetMapping("/form")
 	public String form() {
 		return "eventos/formEvento";
 	}
